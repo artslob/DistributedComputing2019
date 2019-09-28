2f85960cbb721cea114d9d15305b32e8bc5454e0
artslob <artslob@yandex.ru>
2019-09-28 22:09:02 +0300

    add include stdio.h to log.h

--- pa3/log.h
+++ pa3/log.h
@@ -5,6 +5,9 @@
 #define DC_DEBUG_PRINT 0
 #endif
 
+#include <stdio.h>
+
+
 void log_started(FILE *events_log_file, local_id process_id);
 
 void log_received_all_started(FILE *events_log_file, local_id process_id);
@@ -15,8 +18,8 @@ void log_received_all_done(FILE *events_log_file, local_id process_id);
 
 void log_pipe_created(FILE *pipes_log_file, local_id from, local_id to, int read_fd, int write_fd);
 
-void debug_printf(char const * fmt, ...);
+void debug_printf(char const *fmt, ...);
 
-void fatalf(char const * fmt, ...);
+void fatalf(char const *fmt, ...);
 
 #endif // __DC_LOG__H
