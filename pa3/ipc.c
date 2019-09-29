1583a379ff3a2f56eefd21ff601554e267e5c43a
artslob <artslob@yandex.ru>
2019-09-29 16:04:04 +0300

    add enum for ipc operation statuses
    possible statuses:
    success = 0
    error = 1
    no data = 2

--- pa3/ipc.c
+++ pa3/ipc.c
@@ -7,11 +7,17 @@
 #include "pipes.h"
 
 
+typedef enum ReadStatus {
+    RS_SUCCESS = 0,
+    RS_ERROR = 1,
+    RS_NO_DATA = 2,
+} ReadStatus;
+
 int send(void *self, local_id dst, const Message *msg) {
     ProcessContext *context = (ProcessContext *) self;
     pipe_t pipe = get_pipe(context->pipes, context->id, dst);
     int written = write(pipe.write_fd, msg, sizeof(MessageHeader) + msg->s_header.s_payload_len);
-    return (written < 0) ? 1 : 0;
+    return (written < 0) ? RS_ERROR : RS_SUCCESS;
 }
 
 int send_multicast(void *self, const Message *msg) {
@@ -20,41 +26,36 @@ int send_multicast(void *self, const Message *msg) {
         if (i == context->id) {
             continue;
         }
-        if (send(self, i, msg)) {
-            return 1;
+        if (send(self, i, msg) != RS_SUCCESS) {
+            return RS_ERROR;
         }
     }
-    return 0;
+    return RS_SUCCESS;
 }
 
-int blocking_payload_read(int read_fd, Message *msg) {
+ReadStatus blocking_payload_read(int read_fd, Message *msg) {
     const int payload_len = msg->s_header.s_payload_len;
     assert(payload_len >= 0);
     if (payload_len == 0)
-        return 0;
+        return RS_SUCCESS;
 
     while (1) {
         const int count = read(read_fd, msg->s_payload, payload_len);
         if (count < 0 && errno == EAGAIN) {
             continue; // TODO sleep?
         }
-        if (count <= 0 || count != payload_len)
-            return 1;
-        return 0;
+        return (count == payload_len) ? RS_SUCCESS : RS_ERROR;
     }
 }
 
-int asynchronous_header_read(int read_fd, Message *msg) {
+ReadStatus asynchronous_header_read(int read_fd, Message *msg) {
     const int header_size = sizeof(msg->s_header);
+    const int count = read(read_fd, &msg->s_header, header_size);
 
-    int count = read(read_fd, &msg->s_header, header_size);
-    if (count < 0 && errno == EAGAIN) {
-        return -1;
-    }
-    if (count <= 0 || count != header_size) {
-        return 1;
-    }
-    return 0;
+    if (count < 0 && errno == EAGAIN)
+        return RS_NO_DATA;
+
+    return (count == header_size) ? RS_SUCCESS : RS_ERROR;
 }
 
 int receive(void *self, local_id from, Message *msg) {
@@ -62,18 +63,18 @@ int receive(void *self, local_id from, Message *msg) {
     const int read_fd = get_pipe(context->pipes, from, context->id).read_fd;
 
     while (1) {
-        int async_status = asynchronous_header_read(read_fd, msg);
-        if (async_status == -1)
+        ReadStatus status = asynchronous_header_read(read_fd, msg);
+        if (status == RS_NO_DATA)
             continue;
-        if (async_status == 0)
+        if (status == RS_SUCCESS)
             break;
-        if (async_status == 1)
-            return 1;
-        assert(0);
+        if (status == RS_ERROR)
+            return RS_ERROR;
+        assert(0); // should be unreachable
     }
 
     if (msg->s_header.s_magic != MESSAGE_MAGIC)
-        return 1;
+        return RS_ERROR;
 
     return blocking_payload_read(read_fd, msg);
 }
@@ -83,16 +84,16 @@ int receive_any(void *self, Message *msg) {
     while (1) {
         for (local_id from = 0; from < context->N; from++) {
             const int read_fd = get_pipe(context->pipes, from, context->id).read_fd;
-            int async_status = asynchronous_header_read(read_fd, msg);
-            if (async_status == -1)
+            ReadStatus status = asynchronous_header_read(read_fd, msg);
+            if (status == RS_NO_DATA)
                 continue;
-            if (async_status == 1)
-                return 1;
-            assert(async_status == 0);
+            if (status == RS_ERROR)
+                return RS_ERROR;
+            assert(status == RS_SUCCESS);
 
-            if (msg->s_header.s_magic != MESSAGE_MAGIC) {
-                return 1;
-            }
+            if (msg->s_header.s_magic != MESSAGE_MAGIC)
+                return RS_ERROR;
+
             return blocking_payload_read(read_fd, msg);
         }
     }
