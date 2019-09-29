e687c2bb15d2f79d6d0f57fad3e5ecb33ac589b5
artslob <artslob@yandex.ru>
2019-09-29 20:22:20 +0300

    add generic func for receive all with specific type

--- pa3/process_common.c
+++ pa3/process_common.c
@@ -7,29 +7,33 @@
 #include "lamport.h"
 
 
-void receive_all_done(ProcessContext context) {
+static void receive_all_with_type(ProcessContext context, MessageType type) {
     for (local_id from = 1; from < context.N; from++) {
         if (from == context.id)
             continue;
         Message msg;
         assert(receive(&context, from, &msg) == 0);
         assert(msg.s_header.s_magic == MESSAGE_MAGIC);
-        assert(msg.s_header.s_type == DONE);
+        assert(msg.s_header.s_type == type);
         lamport_receive_time(msg.s_header.s_local_time);
-        debug_printf("process %d receive msg with length %lu: %s", context.id, strlen(msg.s_payload), msg.s_payload);
+        switch (type) {
+            case STARTED:
+            case DONE:
+                debug_printf("process %d receive msg with length %lu: %s",
+                             context.id, strlen(msg.s_payload), msg.s_payload);
+                break;
+            default:
+                debug_printf("process %d receive msg with type %d.\n", context.id, type);
+                break;
+        }
     }
 }
 
+void receive_all_done(ProcessContext context) {
+    receive_all_with_type(context, DONE);
+}
+
 void receive_all_started(ProcessContext context) {
-    for (local_id from = 1; from < context.N; from++) {
-        if (from == context.id)
-            continue;
-        Message msg;
-        assert(receive(&context, from, &msg) == 0);
-        assert(msg.s_header.s_magic == MESSAGE_MAGIC);
-        assert(msg.s_header.s_type == STARTED);
-        lamport_receive_time(msg.s_header.s_local_time);
-        debug_printf("process %d receive msg with length %lu: %s", context.id, strlen(msg.s_payload), msg.s_payload);
-    }
+    receive_all_with_type(context, STARTED);
 }
 
