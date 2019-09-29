c0d36fe292a1170000a36320a6c5f273bb2b7afd
artslob <artslob@yandex.ru>
2019-09-29 15:11:18 +0300

    implement receive any

--- pa3/ipc.c
+++ pa3/ipc.c
@@ -65,6 +65,38 @@ int receive(void *self, local_id from, Message *msg) {
 }
 
 int receive_any(void *self, Message *msg) {
-    // TODO implement
-    return 1;
+    ProcessContext *context = (ProcessContext *) self;
+    while (1) {
+        for (local_id from = 0; from < context->N; from++) {
+            const int read_fd = get_pipe(context->pipes, from, context->id).read_fd;
+            const int header_size = sizeof(msg->s_header);
+
+            int count = read(read_fd, &msg->s_header, header_size);
+            if (count < 0 && errno == EAGAIN) {
+                // TODO sleep?
+                continue;
+            }
+            if (count <= 0 || count != header_size) {
+                return 1;
+            }
+            if (msg->s_header.s_magic != MESSAGE_MAGIC) {
+                return 1;
+            }
+            const int payload_len = msg->s_header.s_payload_len;
+            if (payload_len == 0) {
+                return 0;
+            }
+            while (1) {
+                count = read(read_fd, msg->s_payload, payload_len);
+                if (count < 0 && errno == EAGAIN) {
+                    // TODO sleep?
+                    continue;
+                }
+                if (count <= 0 || count != payload_len) {
+                    return 1;
+                }
+                return 0;
+            }
+        }
+    }
 }
