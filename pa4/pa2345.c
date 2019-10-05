afc76594ae1b7eb6badcf1f73ba1b0fe898381f3
artslob <artslob@yandex.ru>
2019-10-05 21:45:42 +0300

    add first version of release_cs

--- pa4/pa2345.c
+++ pa4/pa2345.c
@@ -16,8 +16,21 @@ int request_cs(const void *self) {
     return 0;
 }
 
+static void remove_own_request_from_queue(RequestQueue *queue) {
+    for (int i = 0; i < queue->length - 1; i++) {
+        queue->array[i] = queue->array[i + 1];
+    }
+    queue->length = queue->length - 1;
+}
+
 int release_cs(const void *self) {
     ProcessContext *context = (ProcessContext *) self;
     assert(context != NULL);
+    timestamp_t local_time = lamport_inc_get_time();
+    Message request = {.s_header = {
+            .s_local_time = local_time, .s_type = CS_RELEASE, .s_magic = MESSAGE_MAGIC, .s_payload_len = 0
+    }};
+    remove_own_request_from_queue(&context->queue);
+    assert(send_multicast((void *) self, &request) == 0);
     return 0;
 }
