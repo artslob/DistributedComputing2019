d5627e62d50aef2989894ee5fdace8e7bfd381f7
artslob <artslob@yandex.ru>
2019-10-05 14:49:35 +0300

    make proper output of print_history()

--- pa3/child.c
+++ pa3/child.c
@@ -28,6 +28,8 @@ void child_work(ProcessContext context) {
     close_unused_pipes(context.pipes, context.N, context.id);
     make_pipes_asynchronous(context.pipes, context.N, context.id);
 
+    add_balance_state_to_history(context.balance_history, *context.balance, lamport_get_time(), 0);
+
     send_started(context);
     log_started(context.events_log_fd, context.id);
 
@@ -36,6 +38,7 @@ void child_work(ProcessContext context) {
 
     handle_transfer_requests(context);
 
+    add_balance_state_to_history(context.balance_history, *context.balance, lamport_get_time(), 0);
     send_balance_history_to_parent(context);
 
     close_process_pipes(context.pipes, context.N, context.id);
@@ -80,9 +83,8 @@ void handle_transfer_requests(ProcessContext context) {
         assert(sizeof(order) == request.s_header.s_payload_len);
         memcpy(&order, request.s_payload, request.s_header.s_payload_len);
 
-        timestamp_t time = lamport_inc_get_time();
-
         if (order.s_src == context.id) {
+            timestamp_t time = lamport_inc_get_time();
             *context.balance -= order.s_amount;
             add_balance_state_to_history(context.balance_history, *context.balance, time, order.s_amount);
             request.s_header.s_local_time = time;
@@ -93,9 +95,9 @@ void handle_transfer_requests(ProcessContext context) {
         // current process id must be equal to order's src or dst
         assert(order.s_dst == context.id);
         *context.balance += order.s_amount;
-        add_balance_state_to_history(context.balance_history, *context.balance, time, 0);
+        add_balance_state_to_history(context.balance_history, *context.balance, lamport_get_time(), 0);
         Message ack_for_parent = {.s_header = {
-                .s_magic = MESSAGE_MAGIC, .s_payload_len = 0, .s_type = ACK, .s_local_time = time
+                .s_magic = MESSAGE_MAGIC, .s_payload_len = 0, .s_type = ACK, .s_local_time = lamport_inc_get_time()
         }};
         assert(send(&context, PARENT_ID, &ack_for_parent) == 0);
     }
@@ -114,6 +116,16 @@ void send_balance_history_to_parent(ProcessContext context) {
 
 static void add_balance_state_to_history(BalanceHistory *balance_history, balance_t s_balance, timestamp_t s_time,
                                          balance_t s_balance_pending_in) {
+    if (balance_history->s_history_len > 0) {
+        BalanceState last_balance_state = balance_history->s_history[balance_history->s_history_len - 1];
+        if (last_balance_state.s_time == s_time)
+            return;
+        for (int i = last_balance_state.s_time + 1; i < s_time; i++) {
+            balance_history->s_history[balance_history->s_history_len++] = (BalanceState) {
+                    .s_time=i, .s_balance=last_balance_state.s_balance, .s_balance_pending_in=0
+            };
+        }
+    }
     balance_history->s_history[balance_history->s_history_len++] = (BalanceState) {
             .s_balance=s_balance, .s_time = s_time, .s_balance_pending_in=s_balance_pending_in
     };
