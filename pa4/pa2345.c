992757e40e47acc09267d9a0203491aea578a274
artslob <artslob@yandex.ru>
2019-10-05 22:07:30 +0300

    fix request_cs: send request in message`s payload

--- pa4/pa2345.c
+++ pa4/pa2345.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <string.h>
 
 #include "process_common.h"
 #include "child.h"
@@ -8,11 +9,14 @@
 int request_cs(const void *self) {
     ProcessContext *context = (ProcessContext *) self;
     timestamp_t local_time = lamport_inc_get_time();
-    Message request = {.s_header = {
-            .s_local_time = local_time, .s_type = CS_REQUEST, .s_magic = MESSAGE_MAGIC, .s_payload_len = 0
+    Request request = {.l_time = local_time, .i = context->id};
+    int s_payload_len = sizeof(request);
+    Message request_message = {.s_header = {
+            .s_local_time=local_time, .s_type=CS_REQUEST, .s_magic=MESSAGE_MAGIC, .s_payload_len=s_payload_len
     }};
-    add_request_to_queue(&context->queue, (Request) {.l_time = local_time, .i = context->id});
-    assert(send_multicast((void *) self, &request) == 0);
+    memcpy(request_message.s_payload, &request, s_payload_len);
+    add_request_to_queue(&context->queue, request);
+    assert(send_multicast((void *) self, &request_message) == 0);
     return 0;
 }
 
