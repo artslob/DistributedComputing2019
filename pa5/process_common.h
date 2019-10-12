d1972050c4c60b94e7f55147093a01237660b9e1
artslob <artslob@yandex.ru>
2019-10-12 16:23:59 +0300

    add Fork struct

--- pa5/process_common.h
+++ pa5/process_common.h
@@ -16,6 +16,30 @@ typedef struct {
     Request array[MAX_PROCESS_ID];
 } RequestQueue;
 
+typedef enum {
+    FO_NOT_OWNS = 0,
+    FO_OWNS = 1,
+} FORK_OWNERSHIP;
+
+typedef enum {
+    FS_CLEAN = 0,
+    FS_DIRTY = 1,
+} FORK_STATE;
+
+typedef enum {
+    FR_NOT_REQUESTED = 0,
+    FR_REQUESTED = 1,
+} FORK_REQUEST;
+
+typedef struct {
+    /** Indicates if process have common fork with another process. */
+    FORK_OWNERSHIP ownership;
+    /** Indicates if process have common fork with another process and this fork is dirty or not. */
+    FORK_STATE state;
+    /** Indicates if process have request for common fork with another process. */
+    FORK_REQUEST request;
+} Fork;
+
 typedef struct {
     local_id id;
     local_id N;
@@ -23,6 +47,9 @@ typedef struct {
     FILE *events_log_fd;
     int mutexl;
     RequestQueue queue;
+    /** Always should be equal to N - 1; Defined here only for convenience. */
+    const int fork_length;
+    Fork forks[];
 } ProcessContext;
 
 void receive_all_done(ProcessContext context);
