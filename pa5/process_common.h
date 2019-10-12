fab72666234312755f94dd7bd7c8d5bbd04fec96
artslob <artslob@yandex.ru>
2019-10-12 17:40:03 +0300

    fix: make fork array with fixed length instead of flexible

--- pa5/process_common.h
+++ pa5/process_common.h
@@ -37,11 +37,11 @@ typedef struct {
     pipe_t **pipes;
     FILE *events_log_fd;
     int mutexl;
-    /** Always should be equal to N - 1; Defined here only for convenience. */
+    /** Always should be equal to N - 1. */
     const int fork_length;
     /**
-     * Always should be equal to N - 1. N - 1 is number of child processes. So process will have fork associated with
-     * itself (its index is ProcessContext.id - 1) but such fork should be ignored.
+     * Length always should be equal to N - 1. N - 1 is number of child processes. So process will have fork
+     * associated with itself (its index is ProcessContext.id - 1) but such fork should be ignored.
      * For example, if N = 4 (param -p = 3):
      * Parent`s id is 0.
      * Children`s ids are 1, 2 and 3.
@@ -49,7 +49,7 @@ typedef struct {
      * [ [0]: fork for 1 process; [1]: ignored; [2]: fork for 3 process; ]
      * index 1 is ignored because its fork for 2 process in array of 2 process.
      */
-    Fork forks[];
+    Fork forks[MAX_PROCESS_ID];
 } ProcessContext;
 
 void receive_all_done(ProcessContext context);
